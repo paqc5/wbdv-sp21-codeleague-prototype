b0c37252e66be1a594f791e2c873111c56872022
Paola Quevedo <quevedocontreras.p@husky.neu.edu>
2021-03-28 18:27:00 -0700

    Added cors module

--- src/services/players-service.js
+++ src/services/players-service.js
@@ -1,4 +1,4 @@
-const API_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
+const API_URL = "http://localhost:8080/https://fantasy.premierleague.com/api/bootstrap-static/"
 
 export const findAllPlayers = () => {
   return (
