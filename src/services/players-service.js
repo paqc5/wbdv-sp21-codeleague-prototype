ba1122886bb28c0c021592ef51c17e8aa68e458b
Paola Quevedo <quevedocontreras.p@husky.neu.edu>
2021-03-28 19:00:46 -0700

    Added .env file

--- src/services/players-service.js
+++ src/services/players-service.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:8080/https://fantasy.premierleague.com/api/bootstrap-static/"
+const API_URL = process.env.REACT_APP_FPL_URL
+// const API_URL = "http://localhost:8080/https://fantasy.premierleague.com/api/bootstrap-static/"
 
 export const findAllPlayers = () => {
   return (
